aeab828b16bc5d955fbc1286a5b0c1ebcc34f668
Ossai Reagan <ossaireagano@gmail.com>
2023-06-06 12:13:42 +0100

    opp

--- src/pages/Merge.js
+++ src/pages/Merge.js
@@ -11,7 +11,7 @@ const Merge = () => {
       <hr />
       </div>
       <Home />
-      <hr className="sca" style={{position:'absolute',top:'4015px'}}/>
+      <hr className="sca" style={{position:'absolute',top:'4650px'}}/>
       <Footer />
     </div>
   );
